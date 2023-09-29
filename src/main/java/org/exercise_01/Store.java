3fc38432e6b708fe69bce9151964ee77ea8773d1
natalliakharchenko <morozko.nnn@gmail.com>
2023-09-29 14:50:07 +0200

    Added methods for class Store

---
+++ src/main/java/org/exercise_01/Store.java
@@ -0,0 +1,12 @@
+package org.exercise_01;
+
+public class Store {
+
+    public void addProduct(Product product){
+        //this.products.add(product);
+    }
+
+    public void processOrder(Order order){
+        //this.orders.add(order);
+    }
+}
