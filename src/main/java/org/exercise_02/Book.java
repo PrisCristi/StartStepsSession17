a083b7939c8e756c897e409c93725d853010ba8d
PrisCristi <santos.priscila85@gmail.com>
2023-09-30 19:20:55 +0100

    CewAdded attribuites on class Member

--- src/main/java/org/exercise_02/Book.java
+++ src/main/java/org/exercise_02/Book.java
@@ -1,5 +1,27 @@
 package org.exercise_02;
 
 public class Book {
+    //1. create Attributes
+    private String title;
+    private String author;
+    private Long ISBN;
+
+    // 2. Create method book
+    public Book (String t, String a, Long i){
+        // 3. estabelecer parametros para os atributos. Usar "this".
+        this.title = t;
+        this.author = a;
+        this.ISBN = i;
+    }
+
+    // 3. criar getter metodo para imprimir os info (sobre o produto)
+
+public void getBookInfo(){
+    System.out.println("Title: " + title);
+    System.out.println("Author: " + author);
+    System.out.println("ISBN: " + ISBN);
+}
+
+    // 4. create metodo para saber se o livro está disponível.
 
 }
---
+++ src/main/java/org/exercise_02/Member.java
@@ -0,0 +1,32 @@
+package org.exercise_02;
+
+public class Member {
+    // 1. attributes
+    private String name;
+    private boolean borrowedBooks;
+
+    // 2. getters and setters methods to access member data
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(boolean borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+   // 3. create a method to check if the book was borrowed or not.
+
+
+}
+
+
+
+
